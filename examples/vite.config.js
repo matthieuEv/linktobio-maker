4795e878fc8151388011a3fa6c661a227c9d13d2
matthieuEv <evainmatthieu44@gmail.com>
2024-05-12 19:04:07 +0200

    test

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -1,50 +1,8 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { pluginRollupOptions } from 'vite-plugin-rollup-options';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte(),
-    pluginRollupOptions({
-      plugins: [
-        {
-          name: 'set-javascript-mime-type',
-          rollupOptions: {
-            output: {
-              chunkFileNames: 'assets/chunk-[hash].js',
-              entryFileNames: '[name].js',
-              sourcemap: true,
-              format: 'iife',
-              plugins: [
-                {
-                  name: 'rollup-plugin-inject',
-                  options: {
-                    source: `window.__vite_env = ${JSON.stringify(process.env)}`,
-                  },
-                },
-              ],
-            },
-            plugins: [
-              {
-                name: 'vite-plugin-image-optimizations',
-                options: {
-                  quality: 70,
-                  optimizePNG: true,
-                  optimizeSVG: true,
-                  optimizeJpeg: true,
-                },
-              },
-              {
-                name: 'set-javascript-mime-type',
-                options: {
-                  // Set MIME type for JavaScript files
-                  contentType: 'application/javascript',
-                },
-              },
-            ],
-          },
-        },
-      ],
-    }),
-  ],
-});
+  plugins: [svelte()],
+  base: 'https://matthieuev.github.io/linktobio-maker/',
+})
