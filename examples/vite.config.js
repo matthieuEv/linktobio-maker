5bcd8daffba3550aa7de6dd533d9656d8f92327e
matthieuEv <evainmatthieu44@gmail.com>
2024-05-12 18:59:46 +0200

    test

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -1,10 +1,50 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { pluginRollupOptions } from 'vite-plugin-rollup-options';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    sourcemapInline: true,
-  }
-})
+  plugins: [
+    svelte(),
+    pluginRollupOptions({
+      plugins: [
+        {
+          name: 'set-javascript-mime-type',
+          rollupOptions: {
+            output: {
+              chunkFileNames: 'assets/chunk-[hash].js',
+              entryFileNames: '[name].js',
+              sourcemap: true,
+              format: 'iife',
+              plugins: [
+                {
+                  name: 'rollup-plugin-inject',
+                  options: {
+                    source: `window.__vite_env = ${JSON.stringify(process.env)}`,
+                  },
+                },
+              ],
+            },
+            plugins: [
+              {
+                name: 'vite-plugin-image-optimizations',
+                options: {
+                  quality: 70,
+                  optimizePNG: true,
+                  optimizeSVG: true,
+                  optimizeJpeg: true,
+                },
+              },
+              {
+                name: 'set-javascript-mime-type',
+                options: {
+                  // Set MIME type for JavaScript files
+                  contentType: 'application/javascript',
+                },
+              },
+            ],
+          },
+        },
+      ],
+    }),
+  ],
+});
